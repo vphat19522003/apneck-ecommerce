c08714e0046e1be49035329cce341fe25ede0a49
vphat19522003 <19522003@gm.uit.edu.vn>
2023-07-03 14:59:10 +0700

    first commit

---
+++ src/components/layout.jsx
@@ -0,0 +1,15 @@
+import Header from "./header";
+import Footer from "./footer";
+import { Outlet } from "react-router-dom";
+
+const layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default layout;
